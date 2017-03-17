d568b964d458958d94260de57c962bf4cfee9e3e
Cullen Glassner <Cullen Glassner>
2017-03-16 17:57:25 -0700

    Removed unnecessary Identity Framework links.

--- Startup.cs
+++ Startup.cs
@@ -52,7 +52,7 @@ namespace WebApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}");
             });
         }
     }
